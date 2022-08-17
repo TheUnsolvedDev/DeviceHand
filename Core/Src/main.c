09b944ad8a656a302b869877dea2e8ea1ef22060
Shuvrajeet Das <59395895+TheUnsolvedDev@users.noreply.github.com>
2022-08-17 23:09:37 +0530

    Add files via upload

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,10 +18,15 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "cmsis_os.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include <stdio.h>
+#define MAX 100
 #include "MPU6050.h"
+#include "ssd1306.h"
+#include "fonts.h"
 
 /* USER CODE END Includes */
 
@@ -41,7 +46,16 @@
 
 /* Private variables ---------------------------------------------------------*/
 I2C_HandleTypeDef hi2c1;
+I2C_HandleTypeDef hi2c2;
 
+/* Definitions for ReadMEMS */
+osThreadId_t ReadMEMSHandle;
+const osThreadAttr_t ReadMEMS_attributes = { .name = "ReadMEMS", .stack_size =
+		128 * 4, .priority = (osPriority_t) osPriorityNormal, };
+/* Definitions for DisplayInfo */
+osThreadId_t DisplayInfoHandle;
+const osThreadAttr_t DisplayInfo_attributes = { .name = "DisplayInfo",
+		.stack_size = 128 * 4, .priority = (osPriority_t) osPriorityNormal, };
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
@@ -50,6 +64,10 @@ I2C_HandleTypeDef hi2c1;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_I2C1_Init(void);
+static void MX_I2C2_Init(void);
+void read_mems(void *argument);
+void display_info(void *argument);
+
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -58,14 +76,20 @@ static void MX_I2C1_Init(void);
 /* USER CODE BEGIN 0 */
 RawData_Def myAccelRaw, myGyroRaw;
 ScaledData_Def myAccelScaled, myGyroScaled;
+char buf[MAX];
+int buff_size = 200;
+float acc_queue[200][3] = { { 0 } };
+float gyro_queue[200][3] = { { 0 } };
+
+int buff_read_count = 0;
+int buff_write_count = 0;
 /* USER CODE END 0 */
 
 /**
  * @brief  The application entry point.
  * @retval int
  */
-int main(void)
-{
+int main(void) {
 	/* USER CODE BEGIN 1 */
 	MPU_ConfigTypeDef myMpuConfig;
 	/* USER CODE END 1 */
@@ -89,10 +113,12 @@ int main(void)
 	/* Initialize all configured peripherals */
 	MX_GPIO_Init();
 	MX_I2C1_Init();
+	MX_I2C2_Init();
 	/* USER CODE BEGIN 2 */
 
 	// 1.Initailize Setup
-	MPU6050_Init(&hi2c1);
+	MPU6050_Init(&hi2c2);
+	SSD1306_Init();
 	// 2. Configure Accel and Gyro parameters
 	myMpuConfig.Accel_Full_Scale = AFS_SEL_4g;
 	myMpuConfig.ClockSource = Internal_8MHz;
@@ -100,18 +126,52 @@ int main(void)
 	myMpuConfig.Gyro_Full_Scale = FS_SEL_500;
 	myMpuConfig.Sleep_Mode_Bit = 0; // 1: sleep mode, 0: normal mode
 	MPU6050_Config(&myMpuConfig);
-
 	/* USER CODE END 2 */
 
+	/* Init scheduler */
+	osKernelInitialize();
+
+	/* USER CODE BEGIN RTOS_MUTEX */
+	/* add mutexes, ... */
+	/* USER CODE END RTOS_MUTEX */
+
+	/* USER CODE BEGIN RTOS_SEMAPHORES */
+	/* add semaphores, ... */
+	/* USER CODE END RTOS_SEMAPHORES */
+
+	/* USER CODE BEGIN RTOS_TIMERS */
+	/* start timers, add new ones, ... */
+	/* USER CODE END RTOS_TIMERS */
+
+	/* USER CODE BEGIN RTOS_QUEUES */
+	/* add queues, ... */
+	/* USER CODE END RTOS_QUEUES */
+
+	/* Create the thread(s) */
+	/* creation of ReadMEMS */
+	ReadMEMSHandle = osThreadNew(read_mems, NULL, &ReadMEMS_attributes);
+
+	/* creation of DisplayInfo */
+	DisplayInfoHandle = osThreadNew(display_info, NULL,
+			&DisplayInfo_attributes);
+
+	/* USER CODE BEGIN RTOS_THREADS */
+	/* add threads, ... */
+	/* USER CODE END RTOS_THREADS */
+
+	/* USER CODE BEGIN RTOS_EVENTS */
+	/* add events, ... */
+
+	/* USER CODE END RTOS_EVENTS */
+
+	/* Start scheduler */
+	osKernelStart();
+
+	/* We should never get here as control is now taken by the scheduler */
 	/* Infinite loop */
 	/* USER CODE BEGIN WHILE */
-	while (1)
-	{
-		MPU6050_Get_Accel_Scale(&myAccelScaled);
-		MPU6050_Get_Gyro_Scale(&myGyroScaled);
+	while (1) {
 
-		HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_12);
-		HAL_Delay(10);
 		/* USER CODE END WHILE */
 
 		/* USER CODE BEGIN 3 */
@@ -123,10 +183,9 @@ int main(void)
  * @brief System Clock Configuration
  * @retval None
  */
-void SystemClock_Config(void)
-{
-	RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-	RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+void SystemClock_Config(void) {
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
 	/** Configure the main internal regulator output voltage
 	 */
@@ -145,21 +204,20 @@ void SystemClock_Config(void)
 	RCC_OscInitStruct.PLL.PLLN = 50;
 	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
 	RCC_OscInitStruct.PLL.PLLQ = 7;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-	{
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
 		Error_Handler();
 	}
 
 	/** Initializes the CPU, AHB and APB buses clocks
 	 */
-	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
 	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
 	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
 	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
 	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-	{
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
 		Error_Handler();
 	}
 }
@@ -169,8 +227,7 @@ void SystemClock_Config(void)
  * @param None
  * @retval None
  */
-static void MX_I2C1_Init(void)
-{
+static void MX_I2C1_Init(void) {
 
 	/* USER CODE BEGIN I2C1_Init 0 */
 
@@ -180,7 +237,7 @@ static void MX_I2C1_Init(void)
 
 	/* USER CODE END I2C1_Init 1 */
 	hi2c1.Instance = I2C1;
-	hi2c1.Init.ClockSpeed = 100000;
+	hi2c1.Init.ClockSpeed = 400000;
 	hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
 	hi2c1.Init.OwnAddress1 = 0;
 	hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
@@ -188,13 +245,45 @@ static void MX_I2C1_Init(void)
 	hi2c1.Init.OwnAddress2 = 0;
 	hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
 	hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-	if (HAL_I2C_Init(&hi2c1) != HAL_OK)
-	{
+	if (HAL_I2C_Init(&hi2c1) != HAL_OK) {
 		Error_Handler();
 	}
 	/* USER CODE BEGIN I2C1_Init 2 */
 
 	/* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+ * @brief I2C2 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_I2C2_Init(void) {
+
+	/* USER CODE BEGIN I2C2_Init 0 */
+
+	/* USER CODE END I2C2_Init 0 */
+
+	/* USER CODE BEGIN I2C2_Init 1 */
+
+	/* USER CODE END I2C2_Init 1 */
+	hi2c2.Instance = I2C2;
+	hi2c2.Init.ClockSpeed = 100000;
+	hi2c2.Init.DutyCycle = I2C_DUTYCYCLE_2;
+	hi2c2.Init.OwnAddress1 = 0;
+	hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+	hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+	hi2c2.Init.OwnAddress2 = 0;
+	hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+	hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+	if (HAL_I2C_Init(&hi2c2) != HAL_OK) {
+		Error_Handler();
+	}
+	/* USER CODE BEGIN I2C2_Init 2 */
+
+	/* USER CODE END I2C2_Init 2 */
+
 }
 
 /**
@@ -202,13 +291,14 @@ static void MX_I2C1_Init(void)
  * @param None
  * @retval None
  */
-static void MX_GPIO_Init(void)
-{
-	GPIO_InitTypeDef GPIO_InitStruct = {0};
+static void MX_GPIO_Init(void) {
+	GPIO_InitTypeDef GPIO_InitStruct = { 0 };
 
 	/* GPIO Ports Clock Enable */
-	__HAL_RCC_GPIOD_CLK_ENABLE();
+	__HAL_RCC_GPIOH_CLK_ENABLE();
 	__HAL_RCC_GPIOB_CLK_ENABLE();
+	__HAL_RCC_GPIOD_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
 
 	/*Configure GPIO pin Output Level */
 	HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_RESET);
@@ -219,40 +309,129 @@ static void MX_GPIO_Init(void)
 	GPIO_InitStruct.Pull = GPIO_NOPULL;
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
 	HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 4 */
 
 /* USER CODE END 4 */
 
+/* USER CODE BEGIN Header_read_mems */
+/**
+ * @brief  Function implementing the ReadMEMS thread.
+ * @param  argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_read_mems */
+void read_mems(void *argument) {
+	/* USER CODE BEGIN 5 */
+	/* Infinite loop */
+
+	for (;;) {
+		MPU6050_Get_Accel_Scale(&myAccelScaled);
+		MPU6050_Get_Gyro_Scale(&myGyroScaled);
+
+		acc_queue[buff_read_count % buff_size][0] = myAccelScaled.x;
+		acc_queue[buff_read_count % buff_size][1] = myAccelScaled.y;
+		acc_queue[buff_read_count % buff_size][2] = myAccelScaled.z;
+
+		gyro_queue[buff_read_count % buff_size][0] = myGyroScaled.x;
+		gyro_queue[buff_read_count % buff_size][1] = myGyroScaled.y;
+		gyro_queue[buff_read_count % buff_size][2] = myGyroScaled.z;
+
+		buff_read_count += 1;
+		buff_read_count %= buff_size;
+		osDelay(10);
+	}
+	/* USER CODE END 5 */
+}
+
+/* USER CODE BEGIN Header_display_info */
+/**
+ * @brief Function implementing the DisplayInfo thread.
+ * @param argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_display_info */
+void display_info(void *argument) {
+	/* USER CODE BEGIN display_info */
+	/* Infinite loop */
+	for (;;) {
+		SSD1306_GotoXY(0, 0);
+		SSD1306_Puts("Accel x:", &Font_7x10, 1);
+		SSD1306_GotoXY(0, 1);
+		SSD1306_Puts(gcvt(acc_queue[buff_write_count % buff_size][0], 6, buf),
+				&Font_7x10, 1);
+		SSD1306_GotoXY(1, 0);
+		SSD1306_Puts("Accel y:", &Font_7x10, 1);
+		SSD1306_GotoXY(1, 2);
+		SSD1306_Puts(gcvt(acc_queue[buff_write_count % buff_size][1], 6, buf),
+				&Font_7x10, 1);
+		SSD1306_GotoXY(2, 0);
+		SSD1306_Puts("Accel z:", &Font_7x10, 1);
+		SSD1306_GotoXY(2, 3);
+		SSD1306_Puts(gcvt(acc_queue[buff_write_count % buff_size][2], 6, buf),
+				&Font_7x10, 1);
+
+		SSD1306_GotoXY(10, 30);
+		SSD1306_Puts("WORLD :)", &Font_7x10, 1);
+		SSD1306_UpdateScreen();
+
+		SSD1306_Clear();
+
+		SSD1306_UpdateScreen();
+		buff_write_count += 1;
+		buff_write_count %= buff_size;
+	}
+	/* USER CODE END display_info */
+}
+
+/**
+ * @brief  Period elapsed callback in non blocking mode
+ * @note   This function is called  when TIM6 interrupt took place, inside
+ * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+ * a global variable "uwTick" used as application time base.
+ * @param  htim : TIM handle
+ * @retval None
+ */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+	/* USER CODE BEGIN Callback 0 */
+
+	/* USER CODE END Callback 0 */
+	if (htim->Instance == TIM6) {
+		HAL_IncTick();
+	}
+	/* USER CODE BEGIN Callback 1 */
+
+	/* USER CODE END Callback 1 */
+}
+
 /**
  * @brief  This function is executed in case of error occurrence.
  * @retval None
  */
-void Error_Handler(void)
-{
+void Error_Handler(void) {
 	/* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	__disable_irq();
-	while (1)
-	{
+	while (1) {
 	}
 	/* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef USE_FULL_ASSERT
+#ifdef  USE_FULL_ASSERT
 /**
- * @brief  Reports the name of the source file and the source line number
- *         where the assert_param error has occurred.
- * @param  file: pointer to the source file name
- * @param  line: assert_param error line source number
- * @retval None
- */
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-	/* USER CODE BEGIN 6 */
+  /* USER CODE BEGIN 6 */
 	/* User can add his own implementation to report the file name and line number,
 	   ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-	/* USER CODE END 6 */
+  /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
